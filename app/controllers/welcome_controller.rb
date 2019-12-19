bac596d730220054b31cc7704e243e346c8cc4fa
bbborisk <bbborisk@gmail.com>
2019-12-19 13:00:41 +0100

    added devise and styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!, only:[:index] #zaobilazi before action, kako bi ova strana mogla da se prikaze svakako
   def index
   end
 end
